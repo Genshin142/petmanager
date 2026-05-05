7f0c41ecda7d1d9df512fe09442b5a7f3e1601b5
Genshin142 <362345943@qq.com>
2026-05-05 16:03:07 +0800

    fix: include QAbstractItemView header

--- modules/servicedialog.cpp
+++ modules/servicedialog.cpp
@@ -5,6 +5,7 @@
 #include <QMessageBox>
 #include <QGraphicsDropShadowEffect>
 #include <QPushButton>
+#include <QAbstractItemView>
 
 ServiceDialog::ServiceDialog(QWidget *parent) : QDialog(parent)
 {
